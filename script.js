dc0c32fbc95d01d2f28f8e9f2a893b8664487009
leonardbbacon-coder <leonardbbacon@gmail.com>
2026-07-12 21:34:52 +0800

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ window.onload = () => {
 // TYPEWRITER EFFECT
 // ==========================
 function typeMessage() {
-    const text = "FOR MY LILY OF THE VALLEY ❤️";
+    const text = "For my baby lj";
     const title = document.getElementById("title");
     let i = 0;
     function type() {
